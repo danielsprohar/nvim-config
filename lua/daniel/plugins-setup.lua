c8443bf134671a8be1a40117b8b1a70b51b19931
Daniel Sprohar <daniel.sprohar@gmail.com>
2024-07-09 01:12:01 -0500

    Removed fzf plugin for telescope

--- lua/daniel/plugins-setup.lua
+++ lua/daniel/plugins-setup.lua
@@ -56,7 +56,7 @@ return packer.startup(function(use)
   use("nvim-lualine/lualine.nvim")
 
   -- fuzzy finding w/ telescope
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+  -- use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
   use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
   -- autocompletion
--- lua/daniel/plugins/telescope.lua
+++ lua/daniel/plugins/telescope.lua
@@ -27,4 +27,4 @@ telescope.setup({
   },
 })
 
-telescope.load_extension("fzf")
+-- telescope.load_extension("fzf")
